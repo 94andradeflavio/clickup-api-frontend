21b898063a807c5d47dec4d7f33f59a2c4ec56b7
94andradeflavio <94andrade.flavio@gmail.com>
2023-07-10 16:19:13 -0300

    updated listFunctions with username or email

--- src/helpers/listFunctions.js
+++ src/helpers/listFunctions.js
@@ -13,7 +13,7 @@ export async function searchAndSave(listName) {
       Nome: task.name,
       Objeto: '',
       Diretoria: '',
-      Responsavel: task.assignees.map((assignee) => assignee.username).join(', '),
+      Responsavel: task.assignees.map((assignee) => assignee.username || assignee.email).join(', '),
       DataInicio: task.start_date ? formatDate(new Date(parseInt(task.start_date))) : '',
       DataFim: task.due_date ? formatDate(new Date(parseInt(task.due_date))) : '',
       Status: task.status.status,
