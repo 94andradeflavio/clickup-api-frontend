6bad6523780ecfcb2e6dce904d9f1fe4398d4730
94andradeflavio <94andrade.flavio@gmail.com>
2023-07-10 17:02:39 -0300

    updated serverUrl

--- src/helpers/listFunctions.js
+++ src/helpers/listFunctions.js
@@ -4,6 +4,7 @@ import { generateExcelFile } from './generateExcelFile';
 import { formatDate } from './formatDate';
 
 const serverUrl = 'https://clickup-api-backend.onrender.com';
+// const serverUrl = 'http://localhost:3333';
 
 export async function searchAndSave(listName) {
   try {
@@ -13,6 +14,7 @@ export async function searchAndSave(listName) {
       Nome: task.name,
       Objeto: '',
       Diretoria: '',
+      Gestao: task,
       Responsavel: task.assignees.map((assignee) => assignee.username || assignee.email).join(', '),
       DataInicio: task.start_date ? formatDate(new Date(parseInt(task.start_date))) : '',
       DataFim: task.due_date ? formatDate(new Date(parseInt(task.due_date))) : '',
