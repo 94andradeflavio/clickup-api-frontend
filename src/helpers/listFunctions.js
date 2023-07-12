bb8e6ad268bf89792431125abfe073e68d382672
94andradeflavio <94andrade.flavio@gmail.com>
2023-07-12 08:32:45 -0300

    ADD: new property called 'Criador'

--- src/helpers/listFunctions.js
+++ src/helpers/listFunctions.js
@@ -10,11 +10,12 @@ export async function searchAndSave(listName) {
   try {
     const tasks = await axios.get(`${serverUrl}/${listName}`);
 
-    const data = tasks.data.map((task) => ({
+    const data = await tasks.data.map((task) => ({
       Nome: task.name,
       Objeto: '',
       Diretoria: '',
       Esteira: task.list.name,
+      Criador: task.creator.username || task.creator.email,
       Responsavel: task.assignees.map((assignee) => assignee.username || assignee.email).join(', '),
       DataInicio: task.start_date ? formatDate(new Date(parseInt(task.start_date))) : '',
       DataFim: task.due_date ? formatDate(new Date(parseInt(task.due_date))) : '',
